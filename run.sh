e5ba2dffee67f517fe3bf8308ae590aa89a16448
indrek <indrek@indrek3060>
2021-09-29 10:16:38 +0300

    add lvk

--- run.sh
+++ run.sh
@@ -15,6 +15,7 @@ bin/synthts_et -lex dct/et.dct -lexd dct/et3.dct -o t_lee.wav -f in.txt -m htsvo
 bin/synthts_et -lex dct/et.dct -lexd dct/et3.dct -o t_rna.wav -f in.txt -m htsvoices/eki_et_rna.htsvoice -r 1.1
 bin/synthts_et -lex dct/et.dct -lexd dct/et3.dct -o t_tmb.wav -f in.txt -m htsvoices/eki_et_tmb.htsvoice -r 1.1
 bin/synthts_et -lex dct/et.dct -lexd dct/et3.dct -o t_khn.wav -f in.txt -m htsvoices/eki_et_khn.htsvoice -r 1.1
+bin/synthts_et -lex dct/et.dct -lexd dct/et3.dct -o t_lvk.wav -f in.txt -m htsvoices/eki_et_lvk.htsvoice -r 1.1
 
 #bin/synthts_et -lex dct/et.dct -lexd dct/et3.dct -o t_lsi.wav -f in.txt -m htsvoices/eki_et_lsi.htsvoice -r 1.1
 #bin/synthts_et -lex dct/et.dct -lexd dct/et3.dct -o t_mls.wav -f in.txt -m htsvoices/eki_et_mls.htsvoice -r 1.1
